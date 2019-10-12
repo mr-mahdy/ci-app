588966ddba7c69b4a992aba2f53b785608d5ec0f
mr-mahdy <mr.mahdy5@gmail.com>
2019-10-12 17:00:27 +0700

    menambahkan fungsi tambah dan hapus data CI 2

--- application/models/Mahasiswa_model.php
+++ application/models/Mahasiswa_model.php
@@ -6,4 +6,20 @@ class Mahasiswa_model extends CI_Model
     {
         return $this->db->get_where('mahasiswa')->result_array();
     }
+
+    public function tambahDataMahasiswa()
+    {
+        $data = [
+            "nama" => htmlspecialchars($_POST['nama'], true),
+            "nrp" => htmlspecialchars($_POST['nrp'], true),
+            "email" => htmlspecialchars($_POST['email'], true),
+            "jurusan" => htmlspecialchars($_POST['jurusan'], true),
+        ];
+        $this->db->insert('mahasiswa', $data);
+    }
+
+    public function hapusDataMahasiswa($id)
+    {
+        $this->db->delete('mahasiswa', ['id' => $id]);
+    }
 }
--- application/views/mahasiswa/index.php
+++ application/views/mahasiswa/index.php
@@ -1,9 +1,21 @@
-<div class="container mt-3">
+<div class="container">
+    <?php if ($this->session->flashdata('flash')) : ?>
+    <div class="row mt-3">
+        <div class="col-md-6">
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                Data mahasiswa berhasil <strong><?= $this->session->flashdata('flash'); ?></strong>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        </div>
+    </div>
+    <?php endif; ?>
     <div class="row mb-3">
         <div class="col-lg-6">
-            <button type="button" class="btn btn-primary btnTambahData" data-toggle="modal" data-target="#formModal">
+            <a href="<?= base_url('mahasiswa/tambah') ?>" class="btn btn-primary">
                 Tambah Data Mahasiswa
-            </button>
+            </a>
         </div>
     </div>
     <div class="row">
@@ -13,60 +25,12 @@
                 <?php foreach ($mahasiswa as $mhs) : ?>
                 <li class="list-group-item">
                     <?= $mhs['nama']; ?>
-                    <a href="<?= base_url('mahasiswa/hapus'); ?>" class="badge badge-danger float-right ml-2" onclick="return confirm('yakin?')">hapus</a>
+                    <a href="<?= base_url(); ?>mahasiswa/hapus/<?= $mhs['id'] ?>" class="badge badge-danger float-right ml-2" onclick="return confirm('yakin?')">hapus</a>
                     <a href="<?= base_url('mahasiswa/ubah'); ?>" class="badge badge-success float-right ml-2 tampilModalUbah" data-toggle="modal" data-target="#formModal" data-id="<?= $mhs['id'] ?>">ubah</a>
                     <a href="<?= base_url('mahasiswa/detail'); ?>" class="badge badge-primary float-right">detail</a>
                 </li>
                 <?php endforeach; ?>
             </ul>
-        </div>
-    </div>
-</div>
-
-
-<!-- Modal -->
-<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="judulModal" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form action="<?= base_url() ?>mahasiswa/tambah" method="post">
-                    <input type="hidden" name="id" id="id">
-                    <div class="form-group">
-                        <label for="nama">Nama</label>
-                        <input type="text" class="form-control" name="nama" id="nama">
-                    </div>
-                    <div class="form-group">
-                        <label for="nrp">NRP</label>
-                        <input type="number" class="form-control" name="nrp" id="nrp">
-                    </div>
-                    <div class="form-group">
-                        <label for="email">Email</label>
-                        <input type="email" class="form-control" name="email" id="email">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="jurusan">Jurusan</label>
-                        <select class="form-control" id="jurusan" name="jurusan">
-                            <option value="Teknik Informatika">Teknik Informatika</option>
-                            <option value="Teknik Mesin">Teknik Mesin</option>
-                            <option value="Teknik Industri">Teknik Industri</option>
-                            <option value="Teknik Pangan">Teknik Pangan</option>
-                            <option value="Teknik Planologi">Teknik Planologi</option>
-                            <option value="Teknik Lingkungan">Teknik Lingkungan</option>
-                        </select>
-                    </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="submit" class="btn btn-primary">Tambah Data</button>
-            </div>
-            </form>
         </div>
     </div>
 </div>
