6e8c030c81d5053b697381a08ed51da26d246f5c
mr-mahdy <mr.mahdy5@gmail.com>
2019-10-19 19:45:08 +0700

    menambahkan fitur searching dan pagination CI video 5

--- application/views/peoples/index.php
+++ application/views/peoples/index.php
@@ -1,8 +1,22 @@
 <div class="container">
+    <h3 class="mt-3">List of Peoples</h3>
+
+    <div class="row mb-3">
+        <div class="col-md-5">
+            <form action="<?= base_url('peoples/index'); ?>" method="post">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search Keyword" name="keyword" id="keyword" autocomplete="off" autofocus>
+                    <div class="input-group-append">
+                        <input class="btn btn-primary" type="submit" name="submit">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
     <div class="row">
         <div class="col-md-10">
-            <h3 class="mt-3">List of Peoples</h3>
-
+            <h5>Result : <?= $total_rows; ?></h5>
             <table class="table">
                 <thead>
                     <tr>
@@ -13,6 +27,18 @@
                     </tr>
                 </thead>
                 <tbody>
+                    <?php if (empty($peoples)) : ?>
+                    <tr>
+                        <td colspan="4">
+                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                Data peoples <strong>not found.</strong>
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                        </td>
+                    </tr>
+                    <?php endif; ?>
                     <?php foreach ($peoples as $people) : ?>
                     <tr>
                         <td><?= ++$start; ?></td>
